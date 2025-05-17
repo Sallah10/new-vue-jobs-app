0ef77fc1084c61f6e4f50541c396d8fc3418d83d
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-05-17 22:19:42 +0100

    feat: update Firebase configuration to use environment variables for sensitive data

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCiMbdGpAX5vlbHg5b7VPNryC4m-kTV5pI",
-    authDomain: "vue-jobs-97686.firebaseapp.com",
-    projectId: "vue-jobs-97686",
-    storageBucket: "vue-jobs-97686.firebasestorage.app",
-    messagingSenderId: "650077228173",
-    appId: "1:650077228173:web:a2f1e02195d0c5bf8c66c4",
-    measurementId: "G-S6CJB8DQSM"
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
