e87f88e7a5311330e3ba177c4bc58cfd17adfc8a
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-05-21 16:55:59 +0100

    fix: correct measurementId assignment in Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
     storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.VUE_APP_FIREBASE_APP_ID,
-    measurementId: VUE_APP_FIREBASE_MEASUREMENT_ID
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
