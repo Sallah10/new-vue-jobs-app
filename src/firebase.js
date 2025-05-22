9af014142c7252c06dac526f876588314af49364
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-05-22 16:36:36 +0100

    test case 2

--- src/firebase.js
+++ src/firebase.js
@@ -7,16 +7,23 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyCiMbdGpAX5vlbHg5b7VPNryC4m-kTV5pI",
+    authDomain: "vue-jobs-97686.firebaseapp.com",
+    databaseURL: "https://vue-jobs-97686-default-rtdb.firebaseio.com",
+    projectId: "vue-jobs-97686",
+    storageBucket: "vue-jobs-97686.firebasestorage.app",
+    messagingSenderId: "650077228173",
+    appId: "1:650077228173:web:a2f1e02195d0c5bf8c66c4",
+    measurementId: "G-S6CJB8DQSM"
 };
-
+// apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+// authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+// projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+// databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+// storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+// messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+// appId: import.meta.env.VITE_FIREBASE_APP_ID,
+// measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 // Validate configuration
 if (!firebaseConfig.projectId) {
     throw new Error(
