ae257839e78ada8ade8899dec54b9a0dafb5d1d3
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-05-22 13:01:27 +0100

    fix: update Firebase configuration to use VUE_APP environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: import.meta.env.FIREBASE_API_KEY,
-    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.FIREBASE_APP_ID,
-    measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
+    apiKey: import.meta.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 //VUE_APP_FIREBASE_API_KEY 
 // Initialize Firebase
